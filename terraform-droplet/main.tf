82850c24e3616888543a53b76650518255a52193
Diego <diegopablo012@gmail.com>
2025-07-27 17:17:52 -0600

    full

--- terraform-droplet/main.tf
+++ terraform-droplet/main.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 1.22.1"
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
   }
 }
